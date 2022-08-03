c4cb184fc8dbe42edea110edc7ab9ecf14eecb65
barnden <brandon@nguyen.vc>
2022-08-03 01:59:53 -0500

    Implement better CLI

--- Synthesis.cpp
+++ Synthesis.cpp
@@ -1,30 +1,106 @@
 #include <iostream>
 #include <random>
 
+#include "MultiQuilt.h"
 #include "Transfer.h"
+
+#include <getopt.h>
 
 int main(int argc, char** argv)
 {
-    assert(argc > 1);
+    auto texture_path = std::string {};
+    auto constraint_path = std::string {};
+    auto outfile = std::string {};
 
-    auto texture_name = std::string { argv[1] };
-    auto texture = Image("samples/" + texture_name + ".png");
+    auto method = Quilt::SYNTHESIS_CUT;
+    auto patch_size = 0;
+    auto overlap = 0;
+    auto samples = 0;
+    auto depth = 1;
 
-    if (argc == 5) {
-        auto quilt = Quilt(texture, 384, 384);
+    auto width = 384;
+    auto height = 384;
 
-        quilt.synthesize(atoi(argv[2]), atoi(argv[3]), atoi(argv[4]), Quilt::SYNTHESIS_CUT);
+    option longopts[10] = {
+        option { "texture", 1, NULL, 't' },
+        option { "constraint", 1, NULL, 'c' },
+        option { "outfile", 1, NULL, 'O' },
+        option { "method", 1, NULL, 'm' },
+        option { "patch-size", 1, NULL, 'p' },
+        option { "overlap", 1, NULL, 'o' },
+        option { "samples", 1, NULL, 'K' },
+        option { "width", 1, NULL, 'w' },
+        option { "height", 1, NULL, 'h' },
+        NULL
+    };
 
-        quilt.write("results/" + texture_name + ".png");
-    } else if (argc == 6) {
-        auto constraint_name = std::string { argv[2]};
-        auto constraint = Image("samples/" + constraint_name + ".png");
+    auto option = '\0';
 
+    while ((option = getopt_long(argc, argv, "t:c:O:m:p:o:K:w:h:", longopts, 0)) != -1) {
+        switch (option) {
+        case 't':
+            texture_path = { optarg };
+            break;
+        case 'c':
+            constraint_path = { optarg };
+            break;
+        case 'O':
+            outfile = { optarg };
+            break;
+
+        case 'm':
+            method = atoi(optarg);
+            break;
+        case 'p':
+            patch_size = atoi(optarg);
+            break;
+        case 'l':
+            overlap = atoi(optarg);
+            break;
+        case 'K':
+            samples = atoi(optarg);
+            break;
+        case 'w':
+            width = atoi(optarg);
+            break;
+        case 'h':
+            height = atoi(optarg);
+            break;
+        case 'd':
+            depth = atoi(optarg);
+            break;
+        }
+    }
+
+    if (texture_path.empty())
+        throw std::runtime_error("No texture name supplied.");
+
+    if (outfile.empty())
+        outfile = "output.png";
+
+    if (patch_size <= 0)
+        patch_size = 18;
+
+    if (overlap <= 0)
+        overlap = patch_size / 6;
+
+    if (samples <= 0)
+        samples = 3;
+
+    auto texture = Image(texture_path);
+    if (constraint_path.empty()) {
+        // Texture synthesis if no constraint
+        auto quilt = MultiQuilt(texture, width, height);
+
+        quilt.synthesize(patch_size, overlap, samples, method);
+
+        quilt.write(outfile);
+    } else {
+        auto constraint = Image(constraint_path);
         auto transfer = Transfer(texture, constraint);
 
-        transfer.synthesize(atoi(argv[3]), atoi(argv[4]), atoi(argv[5]));
-
-        transfer.write("results/" + constraint_name + "_" + texture_name + ".png");
+        transfer.synthesize(patch_size, depth, samples);
+        transfer.write(outfile);
     }
 
     return 0;
